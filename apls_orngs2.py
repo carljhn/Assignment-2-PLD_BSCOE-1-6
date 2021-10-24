a0d6e4923d80237ca53b6219230ddff9e3706d66
carljhn <zoletacarljohn1@gmail.com>
2021-10-24 10:52:27 -1200

    second commit

--- apls_orngs2.py
+++ apls_orngs2.py
@@ -3,3 +3,6 @@ oranges = int(input("How many oranges do you want? :"))
 
 amount_apls = apples * 20
 amount_orngs = oranges * 25
+
+total = amount_apls + amount_orngs
+print(f"The total amount is {total}.")
